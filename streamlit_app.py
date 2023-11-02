7bd9451d74c7a4210c5a07e896021a7ac8422a22
kevin6983876 <kevin6983876@gmail.com>
2023-11-02 21:36:45 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,9 @@ import streamlit as st
 """
 data = pd.read_csv('data.csv', sep=',')
 
+date = st.text_input('Date(dd/mm/yyyy)')
 usage = st.number_input('usage in ml', value = 0.0)
 buy = st.number_input('newly added', value = 0.0)
 reset = st.number_input('reset', value = 0.0)
 
+st.button('submit')
