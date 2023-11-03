edba3fa6192f3c0c2c84569468cce2c9a5802a09
kevin6983876 <kevin6983876@gmail.com>
2023-11-03 19:23:55 +0000

    new

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,22 +1,50 @@
-import altair as alt
 import numpy as np
 import pandas as pd
 import streamlit as st
+import datetime
 
 """
 # Chemicals usage tracking app
 """
-operation = st.selectbox('Operation',('Experiment, Purchase, Check stock))
+operation = st.selectbox('Operation',('Experiment', 'Purchase', 'Check stock'))
+st.write('---')
 if operation == 'Experiment':
+    sample = st.number_input('Sample/Detector Diluent (ml)', 0)                                      
+    bead = st.number_input('Bead diluent (ml)', 0)
+    SBG_diluent = st.number_input('SBG diluent (ml)',0)
+    SBG_concentrate = st.number_input(r'SBG concentrate ($\mu$l)',0)
+    disc = st.number_input('Disc',0)
+    plate = st.number_input('Plate',0)
+    RGP = st.number_input('RGP (bottle)', 0)
+    wash_buffer_A = st.number_input('Wash buffer A (pack)',0)
+    wash_buffer_B = st.number_input('Wash buffer B (pack)',0)
+if operation == 'Purchase':
     sample = st.number_input('Sample/Detector Diluent (ml)', 0.0)                                      
     bead = st.number_input('Bead diluent (ml)', 0.0)
+    SBG_diluent = st.number_input('SBG diluent (ml)',0.0)
+    SBG_concentrate = st.number_input(r'SBG concentrate ($\mu$l)',0.0)
+    disc = st.number_input('Disc',0)
+    plate = st.number_input('Plate',0)
+    RGP = st.number_input('RGP (bottle)', 0)
+    wash_buffer_A = st.number_input('Wash buffer A (pack)',0)
+    wash_buffer_B = st.number_input('Wash buffer B (pack)',0)
+if operation == 'Check stock':
+    sample = st.number_input('Sample/Detector Diluent (ml)', 0.0)                                      
+    bead = st.number_input('Bead diluent (ml)', 0.0)
+    SBG_diluent = st.number_input('SBG diluent (ml)',0.0)
+    SBG_concentrate = st.number_input(r'SBG concentrate ($\mu$l)',0.0)
+    disc = st.number_input('Disc',0)
+    plate = st.number_input('Plate',0)
+    RGP = st.number_input('RGP (bottle)', 0)
+    wash_buffer_A = st.number_input('Wash buffer A (pack)',0)
+    wash_buffer_B = st.number_input('Wash buffer B (pack)',0)
+submit = st.button('submit')
+if submit:
+    pass
+
 data = pd.read_csv('data.csv', sep=',')
-st.write('### Current deposit')
+current_datetime = datetime.datetime.now()
+current_date = current_datetime.date()
+current_time = current_datetime.time()
+
 st.dataframe(data)
-
-date = st.text_input('Date(dd/mm/yyyy)')
-usage = st.number_input('usage in ml', value = 0.0)
-buy = st.number_input('newly added', value = 0.0)
-reset = st.number_input('reset', value = 0.0)
-
-option = st.button('submit')
